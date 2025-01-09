0967e59aff7f3890ea4c74da020190d5bfb102c7
Eden Ottley-Hodgson <lh12g22@soton.ac.uk>
2025-01-09 16:22:06 +0000

    fix

--- server-prod.js
+++ server-prod.js
@@ -1,31 +1,35 @@
-import fs from 'node:fs'
-import express from 'express'
-import { setupServer } from './src/server/index.js'
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { Server } from 'socket.io'
-const app = express()
-const server = http.Server(app)
-const io = new Server(server)
+import fs from "node:fs";
+import express from "express";
+import http from "http";
+import { setupServer } from "./src/server/index.js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { Server } from "socket.io";
+const app = express();
+const server = http.Server(app);
+const io = new Server(server);
 
-app.use(express.static(path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist/'), { index: false }));
+app.use(
+	express.static(
+		path.resolve(path.dirname(fileURLToPath(import.meta.url)), "dist/"),
+		{ index: false },
+	),
+);
 
-app.use('*', async (req, res) => {
-  try {
-    const html = fs.readFileSync('./dist/index.html')
-    res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
-  } catch (err) {
-    console.error(err)
-    res.status(500).end(err)
-  }
-})
+app.use("*", async (req, res) => {
+	try {
+		const html = fs.readFileSync("./dist/index.html");
+		res.status(200).set({ "Content-Type": "text/html" }).end(html);
+	} catch (err) {
+		console.error(err);
+		res.status(500).end(err);
+	}
+});
 
 const port = process.env.PORT || 5173;
 
-setupServer(app, io)
+setupServer(app, io);
 
 server.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+	console.log(`http://localhost:${port}`);
 });
-
-
